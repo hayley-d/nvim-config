f5bc8ffe2feb8c26de92f2576f9616001e33c6ac
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2025-04-08 08:02:47 +0200

    Update catppuccin.lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -56,6 +56,37 @@ return {
             vim.cmd.colorscheme("catppuccin")
         end,
     },
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function() 
+        require("gruvbox").setup({
+          terminal_colors = true, -- add neovim terminal colors
+          undercurl = true,
+          underline = true,
+          bold = true,
+          italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+          },
+          strikethrough = true,
+          invert_selection = false,
+          invert_signs = false,
+          invert_tabline = false,
+          invert_intend_guides = false,
+          inverse = true, -- invert background for search, diffs, statuslines and errors
+          contrast = "", -- can be "hard", "soft" or empty string
+          palette_overrides = {},
+          overrides = {},
+          dim_inactive = false,
+          transparent_mode = false,
+        })
+        vim.cmd("colorscheme gruvbox")
+        end
+    },
     -- Rose-pine color scheme setup
     {
         "rose-pine/neovim",
@@ -115,12 +146,14 @@ return {
             vim.api.nvim_create_autocmd("BufEnter", {
                 callback = function()
                     local ft = vim.bo.filetype
-                    if ft ~= "go" and ft ~= "c" then
+                    if ft ~= "go" and ft ~= "ml" then
                         --vim.cmd.colorscheme("rose-pine")
                         vim.cmd.colorscheme("catppuccin")
+                        --vim.cmd.colorscheme("gruvbox")
                     else
                         --vim.cmd.colorscheme("catppuccin")
-                        vim.cmd.colorscheme("rose-pine")
+                        --vim.cmd.colorscheme("rose-pine")
+                        vim.cmd.colorscheme("gruvbox")
                     end
                 end,
             })
